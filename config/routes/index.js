41183b55dcb22e2899d618180b53e82c03d568c5
Rodgers-M <mkishagha@gmail.com>
2017-01-10 11:55:44 +0300

    require the sessioins module

--- config/routes/index.js
+++ config/routes/index.js
@@ -1,7 +1,19 @@
-var express       =  require('express');
-var router        =  express.Router();
-var homeRoutes    =  require('./home');
+var express        =  require('express');
+var router         =  express.Router();
+var homeRoutes     =  require('./home');
+var sessionRoutes  = require('./session');
+
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()){
+    return next();
+  }
+  req.session.returnTo = req.path;
+  res.redirect('/login');
+}
 
 router.get('/', homeRoutes.index);
+router.get('/login', sessionRoutes.new);
+router.get('/logout', sessionRoutes.delete);
+
 
 module.exports = router;
