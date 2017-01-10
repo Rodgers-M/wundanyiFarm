96784986fd0359c08188432e49e9b5081b54095d
Rodgers-M <mkishagha@gmail.com>
2017-01-10 22:43:09 +0300

    add the user Schema

--- app/models/user.js
+++ app/models/user.js
@@ -1,1 +1,10 @@
-//user schema here
+var mongoose      =   require('mongoose');
+var bcrypt        =   require('bcrypt-nodejs');
+var Schema        =   mongoose.Schema;
+
+var userSchema    = new Schema({
+  local            : {
+       email        : String,
+       password     : String,
+   }
+});
