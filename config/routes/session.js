bb1d03379838abab2d2b21d751b8e2d1dff84062
Rodgers-M <mkishagha@gmail.com>
2017-01-10 22:45:54 +0300

    add handlers for the login route

--- config/routes/session.js
+++ config/routes/session.js
@@ -3,9 +3,13 @@ var passport = require('passport');
 module.exports ={
   new : function(req, res){
     res.render('login', {
-        //message: req.flash('loginMessage'),
-        //title: "Login Page"
+        message: req.flash('loginMessage'),
+        title: "Login Page"
     });
+  },
+
+  create : function(req, res){
+    res.send('hello');
   },
 
   delete : function(req, res){
