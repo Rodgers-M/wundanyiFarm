0ff9710c7092de1ed593d2a9a7b361c78160dafd
Rodgers-M <mkishagha@gmail.com>
2017-01-10 22:47:17 +0300

    add handlers for the signup route

--- config/routes/user.js
+++ config/routes/user.js
@@ -3,9 +3,12 @@ var User          = require('../../app/models/user');
 module.exports ={
   new : function(req, res){
     res.render('signup', {
-      //  message : req.flash('signupMessage'),
+       message : req.flash('signupMessage'),
       //  title   : "Sign Up"
     });
   },
+  create : function(req, res){
+      res.send('user created');
+  }
 
 };
