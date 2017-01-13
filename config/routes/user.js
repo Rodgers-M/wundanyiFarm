e7c7cbaa434d67cb6c012842f204da8c10823222
Rodgers-M <mkishagha@gmail.com>
2017-01-13 16:59:38 +0300

    replace username with local.username in the getUserByUsername function

--- config/routes/user.js
+++ config/routes/user.js
@@ -1,5 +1,4 @@
 var User          = require('../../app/models/user');
-
 module.exports ={
   new : function(req, res){
     res.render('signup', {
@@ -8,7 +7,39 @@ module.exports ={
     });
   },
   create : function(req, res){
-      res.send('user created');
+    var user           =  User();
+       user.local.username       =  req.body.username;
+       user.local.firstname      =  req.body.firstname;
+       user.local.lastname       =  req.body.lastname;
+       user.local.email          =  req.body.email;
+       user.local.password       =  req.body.password;
+
+  User.getUserByUsername({'local.username': req.body.username}, function(err, foundUser, done){
+       var message = 'That username is already taken';
+       if(err) throw err;
+
+       if(foundUser){
+        res.render('signup', {
+         message: message
+         });
+
+       } else {
+         console.log('You have no register errors');
+         User.createUser(user,function(err, user){
+           if (err) throw err;
+           res.send('user created');
+          /* req.login(user, function(err){
+             if (!err){
+               console.log(user);
+               res.redirect('/');
+               //res.send(user);
+             } else{
+               console.log("there was an error ", err);
+             }
+           });*/
+         });
+       }
+     });
   }
 
 };
