760e4d65e2c48d6c2c038d0c878b674468fb65f3
Rodgers-M <mkishagha@gmail.com>
2017-01-24 11:56:25 +0300

    add the logout route

--- config/routes/index.js
+++ config/routes/index.js
@@ -1,4 +1,5 @@
 var express        =  require('express');
+var passport       =  require('passport');
 var router         =  express.Router();
 var homeRoutes     =  require('./home');
 var sessionRoutes  = require('./session');
@@ -14,9 +15,19 @@ function isLoggedIn(req, res, next) {
 
 router.get('/', homeRoutes.index);
 router.get('/login', sessionRoutes.new);
-router.post('/session/create', sessionRoutes.create);
+router.post('/session/create',
+      passport.authenticate('local-login', {
+      successRedirect : '/', // redirect to the secure profile section
+      failureRedirect : '/login', // redirect back to the signup page if there is an error
+      failureFlash : true // allow flash messages
+  })
+);
 router.get('/logout', sessionRoutes.delete);
 router.get('/signup', userRoutes.new );
-router.post('/user/create', userRoutes.create);
+router.post('/user/create',passport.authenticate('local-signup', {
+        successRedirect : '/login', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
 module.exports = router;
