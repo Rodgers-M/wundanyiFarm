a2d2d9bc955785f4110ab4880e0863d4b5ad79fa
Rodgers-M <mkishagha@gmail.com>
2017-01-24 11:57:32 +0300

    define what should happen during logout

--- config/routes/session.js
+++ config/routes/session.js
@@ -1,5 +1,3 @@
-var passport = require('passport');
-
 module.exports ={
   new : function(req, res){
     res.render('login', {
@@ -7,11 +5,6 @@ module.exports ={
         title: "Login Page"
     });
   },
-
-  create : function(req, res){
-    res.send('hello, you have logged in successfully');
-  },
-
   delete : function(req, res){
     req.logout();
     res.redirect('/');
