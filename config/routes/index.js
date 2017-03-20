491ecdbcaa62bf7632c6913fd17efb2e405ea3cb
Rodgers-M <mkishagha@gmail.com>
2017-03-20 18:03:49 +0300

    remove the definitions of what happens when a user is created/authenticated from this file and define the same in user.js and session.js respectively.

--- config/routes/index.js
+++ config/routes/index.js
@@ -5,33 +5,25 @@ var homeRoutes     =  require('./home');
 var sessionRoutes  = require('./session');
 var userRoutes     = require('./user');
 var animalRoutes   = require('./animal');
+//check if the user is loggedin or not
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()){
     return next();
   }
   req.session.returnTo = req.path;
+  req.flash('info', 'you must login to acces this page');
   res.redirect('/login');
 }
 
 router.get('/', homeRoutes.index);
 router.get('/login', sessionRoutes.new);
-router.post('/session/create',
-      passport.authenticate('local-login', {
-      successRedirect : '/viewanimals',
-      failureRedirect : '/login', // redirect back to the signup page if there is an error
-      failureFlash : true // allow flash messages
-  })
-);
+router.post('/session/create',sessionRoutes.create);
 router.get('/logout', sessionRoutes.delete);
 router.get('/signup', userRoutes.new );
-router.post('/user/create',passport.authenticate('local-signup', {
-        successRedirect : '/viewanimals', // redirect to the secure profile section
-        failureRedirect : '/signup', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
-router.get('/records', animalRoutes.records);
-router.post('/animalnew', animalRoutes.create);
-router.get('/viewanimals', animalRoutes.index);
-router.post('/healthcreate', animalRoutes.healthcreate);
+router.post('/user/create', userRoutes.create);
+router.get('/records',isLoggedIn, animalRoutes.records);
+router.post('/animalnew',isLoggedIn, animalRoutes.create);
+router.get('/viewanimals',isLoggedIn, animalRoutes.index);
+router.post('/healthcreate',isLoggedIn, animalRoutes.healthcreate);
 
 module.exports = router;
