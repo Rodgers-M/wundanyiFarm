410654ea43ada4165487912f69600216bdfefb25
Rodgers-M <mkishagha@gmail.com>
2017-03-20 18:05:05 +0300

    define what happens when a user is authenticated in the 'create' function

--- config/routes/session.js
+++ config/routes/session.js
@@ -1,9 +1,23 @@
+var passport   =  require('passport');
 module.exports ={
   new : function(req, res){
     res.render('login', {
         message: req.flash('loginMessage'),
         title: "Login Page"
     });
+  },
+  create : function(req, res, next){
+    passport.authenticate('local-login', function(err, user){
+      if(err) return next(err);
+      if(!user){
+        console.log('user not found');
+       return res.redirect('/login');
+     }
+     req.login(user, function(err){
+       console.log(user);
+       res.redirect(req.session.returnTo || '/viewanimals');
+     });
+    })(req, res, next);
   },
   delete : function(req, res){
     req.logout();
