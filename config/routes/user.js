4785359865ae94de2531f8b995cdc62afb31b160
Rodgers-M <mkishagha@gmail.com>
2017-03-20 18:10:06 +0300

    define what happens when a user is created in the 'create' function

--- config/routes/user.js
+++ config/routes/user.js
@@ -1,3 +1,4 @@
+var passport   =  require('passport');
 var User          = require('../../app/models/user');
 module.exports ={
   new : function(req, res){
@@ -5,5 +6,12 @@ module.exports ={
        message : req.flash('signupMessage'),
        title   : "Sign Up"
     });
+  },
+  create : function(req, res, next){
+    passport.authenticate('local-signup',{
+      successRedirect : '/viewanimals',
+      failureRedirect : '/signup',
+      failureFlash : true // allow flash messages
+    })(req, res, next);
   }
 };
