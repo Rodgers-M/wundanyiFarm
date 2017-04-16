fae81670f334574067e0afd792a27256ff50ec9e
Rodgers-M <mkishagha@gmail.com>
2017-04-16 15:04:13 +0300

    change the flash message of the case where user is not found

--- config/passport.js
+++ config/passport.js
@@ -29,20 +29,20 @@ passport.use('local-signup', new LocalStrategy({
        // User.findOne wont fire unless data is sent back
        process.nextTick(function() {
 
-       // find a user whose email is the same as the forms email
+       // find a user whose username is the same as the forms username
        // we are checking to see if the user trying to login already exists
        User.findOne({ 'local.username' :  username }, function(err, user) {
            // if there are any errors, return the error
            if (err)
                return done(err);
 
-           // check to see if theres already a user with that email
+           // check to see if theres already a user with that username
            if (user) {
                return done(null, false,
                  req.flash('error', 'That username is already taken.'));
            } else {
 
-               // if there is no user with that email
+               // if there is no user with that username
                // create the user
                var newUser            = new User();
 
@@ -68,14 +68,13 @@ passport.use('local-signup', new LocalStrategy({
    }));
 
 passport.use('local-login', new LocalStrategy({
-    // by default, local strategy uses username and password, we will override with email
     usernameField : 'username',
     passwordField : 'password',
     passReqToCallback : true // allows us to pass back the entire request to the callback
 },
-function(req, username, password, done) { // callback with email and password from our form
+function(req, username, password, done) { // callback with username and password from our form
 
-    // find a user whose email is the same as the forms email
+    // find a user whose username is the same as the forms username
     // we are checking to see if the user trying to login already exists
     User.findOne({ 'local.username' :  username }, function(err, user) {
         // if there are any errors, return the error before anything else
@@ -84,7 +83,7 @@ function(req, username, password, done) { // callback with email and password fr
 
         // if no user is found, return the message
         if (!user)
-            return done(null, false, req.flash('error', 'No user with username  "', username, '"  found.')); // req.flash is the way to set flashdata using connect-flash
+            return done(null, false, req.flash('error', 'User not  found.'));
 
         // if the user is found but the password is wrong
         if (!user.validPassword(password))
