5e35e85f6cad14dd55cd95f65c3dcb9a482698c6
Rodgers-M <mkishagha@gmail.com>
2017-04-18 16:51:34 +0300

    remove the health deletion from the user schema. it will be implemented in animal schema

--- app/models/user.js
+++ app/models/user.js
@@ -28,9 +28,8 @@ userSchema.methods.validPassword = function(password) {
 //delete all documents referenced to the user before deleteing the user
 //this will prevent deletion anomalies
 userSchema.pre('remove', function(next){
-	Animal.remove({owner	 : this.local.username}).exec();
-	Health.remove({owner	 : this.local.username}).exec();
-	FarmInput.remove({owner	 : this.local.username}).exec();
+	Animal.remove({'owner'	 : this.local.username}).exec();
+	FarmInput.remove({'owner'	 : this.local.username}).exec();
 });
 // create the model for users and expose it to our app
 module.exports = mongoose.model('User', userSchema);
