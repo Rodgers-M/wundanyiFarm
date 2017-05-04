6d390b6bf5c2126cdbff225179dfde53247828d0
Rodgers-M <mkishagha@gmail.com>
2017-05-04 17:45:04 +0300

    modify the create function to include the totalcost field

--- config/routes/farminput.js
+++ config/routes/farminput.js
@@ -6,19 +6,28 @@ module.exports = {
 	});
   },
   create : function(req, res){
+	var totalcost = req.body.NumOfItems * req.body.itemprice;
     var  farminput = new FarmInput();
 
     farminput.itemBought    = req.body.item;
     farminput.NumOfItems    = req.body.NumOfItems;
-    farminput.pricePerItem  = req.body.price;
+    farminput.pricePerItem  = req.body.itemprice;
     farminput.date          = req.body.date;
     farminput.owner         = req.user.username;
-
-    farminput.save(function(err, record){
-      if(err) return err;
+	farminput.totalcost		= totalcost;
+	console.log(farminput);
+    farminput.save(function(error, record){
+      if(error) return(error);
       req.flash('success', 'record saved successfuly');
       res.redirect('/farminput');
       console.log(record);
-    })
-  }
+    });
+  },
+	index : function(req, res){
+		var records = [];
+		res.render('farminput/index',{
+		 page : 'farminput',
+		records:records
+		});
+	}
 }
