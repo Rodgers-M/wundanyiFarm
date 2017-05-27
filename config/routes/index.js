d015104e98ae96ecba78c9782601babbf342bd93
Rodgers-M <mkishagha@gmail.com>
2017-05-27 15:56:42 +0300

    add routes for the update and edit of farm inputs

--- config/routes/index.js
+++ config/routes/index.js
@@ -16,23 +16,35 @@ function isLoggedIn(req, res, next) {
   req.flash('info', 'you must login to acces this page');
   res.redirect('/login');
 }
-
+// user routes
 router.get('/', homeRoutes.index);
 router.get('/login', sessionRoutes.new);
 router.post('/session/create',sessionRoutes.create);
 router.get('/logout', sessionRoutes.delete);
 router.get('/signup', userRoutes.new );
 router.post('/user/create', userRoutes.create);
+
+//animal routes
 router.get('/records',isLoggedIn, animalRoutes.records);
 router.post('/animalnew',isLoggedIn, animalRoutes.create);
 router.get('/viewanimals',isLoggedIn, animalRoutes.index);
 router.post('/healthcreate',isLoggedIn, animalRoutes.healthcreate);
 router.get('/animalhealth',isLoggedIn,  animalRoutes.viewhealth);
-router.get('/newfarminput', isLoggedIn, farminputRoutes.new);
-router.post('/inputcreate', isLoggedIn, farminputRoutes.create);
-router.get('/farminput', isLoggedIn, farminputRoutes.index);
 router.get('/editanimal/:tagnum',isLoggedIn, animalRoutes.edit);
 router.post('/animals/update', isLoggedIn, animalRoutes.update);
 router.post('/deleteanimal', isLoggedIn, animalRoutes.delete);
+
+//farm input routes
+router.get('/newfarminput', isLoggedIn, farminputRoutes.new);
+router.post('/inputcreate', isLoggedIn, farminputRoutes.create);
+router.get('/farminput', isLoggedIn, farminputRoutes.index);
+router.get('/editinput/:slug', isLoggedIn, farminputRoutes.edit);
+router.post('/updateinput', isLoggedIn, farminputRoutes.update);
+router.get('/deleteinput', isLoggedIn, farminputRoutes.delete);
+
+
+//Error handler for any other route apart from the defined routes
 router.use(errorhandler.notfound);
+
+//export the router for use by the app
 module.exports = router;
