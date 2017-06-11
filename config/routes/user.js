9b798e76eb88e415ea409046f7b7686744ecfbba
Rodgers-M <mkishagha@gmail.com>
2017-06-11 16:14:24 +0300

    add functions to delete a user and view all users

--- config/routes/user.js
+++ config/routes/user.js
@@ -13,5 +13,22 @@ module.exports ={
       failureRedirect : '/signup',
       failureFlash : true // allow flash messages
     })(req, res, next);
-  }
+  },
+  index : function(req, res){
+     User.find({}, function(error, users){
+       if(error) res.send(error);
+       res.render('admindashboard/users',{
+           users  : users,
+           page   : 'users'
+       });
+     });
+   },
+	delete :function(req, res){
+		User.findOneAndRemove({'local.username' : req.params.user}, function(error, removedUser){
+			if(error) return next(error);
+			req.flash('success', 'User deleted successfully');
+			res.redirect('/agrimonadmin');
+		});
+	
+	}
 };
