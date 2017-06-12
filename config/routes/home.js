8dd3dbde8256170a0b68d280d573584f85a61984
Rodgers-M <mkishagha@gmail.com>
2017-06-12 09:00:26 +0300

    add a route to the farm status page

--- config/routes/home.js
+++ config/routes/home.js
@@ -2,5 +2,10 @@ module.exports = {
   index : function(req, res){
       res.render('index'); //load the homepage
 
-}
+},
+	status : function(req, res){
+		res.render('admindashboard/farmstatus',
+				{page : 'farmstatus'}
+				);
+	}
 };
