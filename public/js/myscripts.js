038e377a23b339dc09732aa24922067cece4b735
Rodgers-M <mkishagha@gmail.com>
2017-06-12 09:03:44 +0300

    remove the auto add of sticky class on scroll

--- public/js/myscripts.js
+++ public/js/myscripts.js
@@ -1,16 +1,2 @@
-$(document).ready(function(){
 
-	$('.js-content').waypoint(function(direction){
-
-		if(direction=="down"){
-
-			$('#stickydiv').addClass('stickynav');
-		} else{
-			$('#stickydiv').removeClass('stickynav');
-		}
-	} , {
-		//offset: '40%;'
-	});
-
-});
 $('.close-button').closest('[data-closable]').fadeOut(6000);
